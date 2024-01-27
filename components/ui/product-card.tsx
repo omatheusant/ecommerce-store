11dbe398fa2dba93622f7c3cf27da924d0be1fd0
Matheus <mat.alvessantana@gmail.com>
2024-01-27 10:36:10 -0300

    update: getProducts and featured products

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import { Product } from "@/types";
 import Image from "next/image";
+import { Expand, ShoppingCart } from "lucide-react";
+
+import { Product } from "@/types";
+import IconButton from "@/components/ui/icon-button";
+import Currency from "@/components/ui/currency";
 
 interface ProductCardProps {
   data: Product;
@@ -17,9 +21,36 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
           fill
           className="aspect-square oject-cover rounded-md"
         />
-        <div className="opacity-0 group-hover:opacity-100">
-
+        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
+          <div className="flex gap-x-6 justify-center">
+            <IconButton
+              onClick={() => {}}
+              icon={
+                <Expand
+                  size={20}
+                  className="text-gray-600"
+                />
+              }
+            />
+            <IconButton
+              onClick={() => {}}
+              icon={
+                <ShoppingCart
+                  size={20}
+                  className="text-gray-600"
+                />
+              }
+            />
+          </div>
         </div>
+      </div>
+      {/* Description */}
+      <div>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category.name}</p>
+      </div>
+      <div className="flex items-center justify-between">
+            <Currency value={data.price}/>
       </div>
     </div>
   );
